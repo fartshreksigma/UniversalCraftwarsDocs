4edf41c263a7ce2c8378947be8b3828c3809912d
fart_shrek <fart.shrek1@gmail.com>
2024-08-31 20:40:30 +1000

    Built and published by Moonwave

---
+++ assets/js/8c07c76e.853e93ea.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[381],{63891:s=>{s.exports=JSON.parse('{"sourceUrl":"https://github.com/fartshreksigma/UniversalCraftwarsDocs/blob/master","baseUrl":"/UniversalCraftwarsDocs/","classOrder":[],"apiCategories":[]}')}}]);
---
+++ assets/js/baaddf41.1f72d984.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[923],{78933:e=>{e.exports=JSON.parse('[{"value":"Types","id":"types","level":2},{"value":"DataTemplate","id":"DataTemplate","level":3},{"value":"Properties","id":"properties","level":2},{"value":"Functions","id":"functions","level":2}]')}}]);
