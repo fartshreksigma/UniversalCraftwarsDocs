966e79401ea239ae95b402e523ab6c10684cdeda
fart_shrek <fart.shrek1@gmail.com>
2024-09-03 10:42:05 +1000

    Built and published by Moonwave

--- assets/js/baaddf41.a3a66eee.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[923],{78933:e=>{e.exports=JSON.parse('[{"value":"Types","id":"types","level":2},{"value":"DataTemplate","id":"DataTemplate","level":3},{"value":"Properties","id":"properties","level":2},{"value":"Functions","id":"functions","level":2},{"value":":GetTemplate","id":"GetTemplate","level":3}]')}}]);
